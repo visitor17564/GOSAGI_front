befcdc2fa74fed9118a94e5f62d404b97e7a7e55
visitor17564 <lanlanloo17564@gmail.com>
2024-01-27 01:17:56 +0900

    🎉 init : 로그아웃 및 메뉴삭제기능 후가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
 const signupButton = document.getElementById('signup-button');
 const loginButton = document.getElementById('login-button');
+const logoutButton = document.getElementById('logout');
+
+const loginDiv = document.getElementById('login');
+const loginBar = document.getElementById('login-bar');
+const signupDiv = document.getElementById('signup');
+const signupBar = document.getElementById('signup-bar');
+const logoutDiv = document.getElementById('logout');
+const logoutBar = document.getElementById('logout-bar');
+const myPageDiv = document.getElementById('my-page');
+
+let isLoggedIn = false;
+
+if (document.cookie.includes('authorization')) {
+  isLoggedIn = true;
+} else {
+  isLoggedIn = false;
+}
+
+console.log(isLoggedIn);
+
+updateLoginButton(isLoggedIn);
+
+// 로그인 상태에 따라 로그인 버튼의 표시 여부를 업데이트하는 함수
+function updateLoginButton() {
+  if (isLoggedIn) {
+    // 로그인 상태인 경우, 로그인 버튼을 숨깁니다.
+    loginDiv.style.display = 'none';
+    loginBar.style.display = 'none';
+    signupDiv.style.display = 'none';
+    signupBar.style.display = 'none';
+    myPageDiv.style.display = 'block';
+    logoutDiv.style.display = 'block';
+    logoutBar.style.display = 'block';
+  } else {
+    // 로그인 상태가 아닌 경우, 로그인 버튼을 표시합니다.
+    loginDiv.style.display = 'block';
+    loginBar.style.display = 'block';
+    signupDiv.style.display = 'block';
+    signupBar.style.display = 'block';
+    myPageDiv.style.display = 'none';
+    logoutDiv.style.display = 'none';
+    logoutBar.style.display = 'none ';
+  }
+}
+
 let page = 1;
 
 export async function signup() {
@@ -138,4 +183,15 @@ signupButton.addEventListener('click', () => {
 
 loginButton.addEventListener('click', () => {
   login();
+});
+
+logoutButton.addEventListener('click', async () => {
+  try {
+    const response = await axios.get(`http://localhost:3000/auth/logout`, { withCredentials: true });
+    alert('로그아웃 성공');
+    window.location.href = 'http://localhost:5500/html/index.html'; // 수정할 URL로 변경 필요
+  } catch (err) {
+    // 오류 처리
+    alert('오류발생: ' + err);
+  }
 });
