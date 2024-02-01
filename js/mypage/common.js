147e5b8291c0be287829fb58e535e14e3e1767c8
ay0530 <edkdud530@gmail.com>
2024-02-01 10:54:44 +0900

    ✨ feat: 회원 닉네임 조회 기능 활성화

--- js/mypage/common.js
+++ js/mypage/common.js
@@ -2,23 +2,24 @@
 const $nicknameFix = document.getElementById('nickname-fix');
 const $secessionBtn = document.getElementById('secession-btn');
 
-// 회원 로그인 여부 체크
-// document.addEventListener('DOMContentLoaded', async function () {
-//   try {
-//     // 회원정보 조회 API 실행
-//     const response = await axios.get('http://localhost:3000/user', {
-//       withCredentials: true,
-//     });
+// 회원 로그인 여부 체크;
+document.addEventListener('DOMContentLoaded', async function () {
+  try {
+    // 회원정보 조회 API 실행
+    const response = await axios.get('http://localhost:3000/user', {
+      withCredentials: true,
+    });
 
-//     $nicknameFix.innerText = response.data.data.nickname;
-//   } catch (err) {
-//     // 오류 처리
-//     alert(`${err.response.data.message}`);
-//     if (err.response.data.message === "로그인을 진행해주세요.") {
-//       window.location.href = "http://localhost:5500/html/index.html";
-//     }
-//   }
-// });
+    $nicknameFix.innerText = response.data.data.nickname;
+  } catch (err) {
+    console.log('err: ', err);
+    // 오류 처리
+    // alert(`${err.response.data.message}`);
+    // if (err.response.data.message === "로그인을 진행해주세요.") {
+    //   window.location.href = "http://localhost:5500/html/index.html";
+    // }
+  }
+});
 
 
 // 내 정보 수정
