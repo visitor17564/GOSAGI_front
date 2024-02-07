9d6e6d121182821b00d511be6ba5effceab09262
visitor17564 <lanlanloo17564@gmail.com>
2024-02-07 19:46:21 +0900

    🩹 fix: 마이페이지 오류수정, 마이페이지단 헤더 마저 수정

--- js/mypage/auth.js
+++ js/mypage/auth.js
@@ -1,25 +1,19 @@
 const logoutButton = document.getElementById('logout');
+const $secessionBtn = document.getElementById('secession-btn');
 
-// if (!document.cookie.includes('authorization')) {
-//   alert('로그인 후 이용 가능합니다.');
-//   // window.location.href = './';
-// }
-
-// 회원 로그인 여부 체크
 document.addEventListener('DOMContentLoaded', async function () {
   try {
-    // 회원정보 조회 API 실행
-    const response = await axios.get('https://back.gosagi.com/user', {
+    // 장바구니조회 API 실행
+    const response = await axios.get('https://back.gosagi.com/cart', {
       withCredentials: true,
     });
-
-    $nicknameFix.innerText = response.data.data.nickname;
+    const cartsCount = response.data.data.cart_count;
+    drawCartCount(cartsCount);
+    isLoggedIn = true;
+    // 조회된 정보 적용
   } catch (err) {
-    // 오류 처리
-    alert(`${err.response.data.message}`);
-    if (err.response.data.message === '로그인을 진행해주세요.') {
-      window.location.href = './';
-    }
+    alert('로그인을 진행해주세요');
+    window.location.href = '/';
   }
 });
 
@@ -27,9 +21,31 @@ logoutButton.addEventListener('click', async () => {
   try {
     const response = await axios.post(`https://back.gosagi.com/auth/logout`, {}, { withCredentials: true });
     alert('로그아웃 성공');
-    window.location.href = './'; // 수정할 URL로 변경 필요
+    window.location.href = '/'; // 수정할 URL로 변경 필요
   } catch (err) {
     // 오류 처리
     alert('오류발생: ' + err.response.data.message);
   }
 });
+
+// 회원탈퇴
+$secessionBtn.addEventListener('click', async function (event) {
+  if (confirm('정말 탈퇴하시겠습니까?')) {
+    try {
+      // 회원 탈퇴 API 실행
+      const response = await axios.delete('https://back.gosagi.com/user', {
+        withCredentials: true,
+      });
+
+      alert(response.data.message);
+      window.location.href = '/';
+    } catch (err) {
+      // 오류 처리
+      alert(err.response.data.message);
+    }
+  }
+});
+
+export function drawCartCount(cartsCount) {
+  cartDiv.innerHTML = `<button class="text-center text-white text-xs font-normal font-['Inter']"><a href="/html/mypage/cart.html">장바구니(${cartsCount})</a></button>`;
+}
